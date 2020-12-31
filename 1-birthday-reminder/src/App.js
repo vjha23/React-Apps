6f515331589110080e8ab336a27ef53002db20e1
Vijay Jha <51393417+vjha23@users.noreply.github.com>
2020-12-31 19:48:37 +0530

    Add files via upload

---
+++ 1-birthday-reminder/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <React.Fragment>
+      <main>
+        <section className='container'>
+          <h3>{people.length} birthday today</h3>
+          <List people={people} />
+          <button className='btn' onClick={() => setPeople([])}>
+            clear all
+            </button>
+        </section>
+      </main>
+    </React.Fragment>
+  )
+}
+
+export default App;
